15683949146b433d835260fb33acd56691c13d5e
bala-amypo <bala@amypo.in>
2025-12-20 06:08:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/service.impl/FertilizerSeviceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Fertilizer;
+import com.example.demo.repository.FertilizerRepository;
+import com.example.demo.service.FertilizerService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FertilizerServiceImpl implements FertilizerService {
+
+    private final FertilizerRepository fertilizerRepository;
+
+    public FertilizerServiceImpl(FertilizerRepository fertilizerRepository) {
+        this.fertilizerRepository = fertilizerRepository;
+    }
+
+    @Override
+    public Fertilizer addFertilizer(Fertilizer fertilizer) {
+        return fertilizerRepository.save(fertilizer);
+    }
+
+    @Override
+    public List<Fertilizer> getAllFertilizers() {
+        return fertilizerRepository.findAll();
+    }
+
+    @Override
+    public Fertilizer getFertilizerById(Long id) {
+        return fertilizerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Fertilizer not found"));
+    }
+}
