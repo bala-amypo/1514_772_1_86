3e7c8d54f651d437b8d359bd555c563304c54c95
bala-amypo <bala@amypo.in>
2025-12-26 04:28:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/Crop.java
+++ src/main/java/com/example/demo/entity/Crop.java
@@ -1,23 +1,41 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "crops")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Crop {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Crop name is required")
+    @NotBlank
     private String name;
 
-    @NotBlank(message = "Season is required")
+    @NotNull
+    private Double suitablePHMin;
+
+    @NotNull
+    private Double suitablePHMax;
+
+    @NotNull
+    private Double requiredWater;
+
+    @NotBlank
     private String season;
 }
