6eee110f0164abc8afc4a74f469446958f8f2e53
bala-amypo <bala@amypo.in>
2025-12-26 17:04:28 +0000

    User commit

--- src/main/java/com/example/demo/service/FarmService.java
+++ src/main/java/com/example/demo/service/FarmService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.FarmRequest;
+import com.example.demo.entity.Farm;
+import java.util.List;
 
 public interface FarmService {
-
-    Object createFarm(FarmRequest farmRequest, Long userId);
-
-    Object getFarms(Long userId);
+    Farm createFarm(Farm farm, Long userId);
+    Farm getFarmById(Long id);
+    List<Farm> getFarmsByOwner(Long ownerId);
 }
