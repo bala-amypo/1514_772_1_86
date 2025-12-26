962d601505e2cd48e5dbe52be79356b437b6c640
bala-amypo <bala@amypo.in>
2025-12-26 05:19:31 +0000

    User commit

--- src/main/java/com/example/demo/dto/FertilizerRequest.java
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,9 +10,12 @@ public class FertilizerRequest {
     @NotBlank(message = "Fertilizer name is required")
     private String name;
 
-    @NotBlank(message = "NPK ratio is required")
+    @Pattern(
+        regexp = "^\\d+-\\d+-\\d+$",
+        message = "NPK format must be N-P-K"
+    )
     private String npkRatio;
 
-    @NotBlank(message = "Recommended crops field is required")
+    @NotBlank(message = "Recommended crops are required")
     private String recommendedForCrops;
 }
